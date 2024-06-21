5eb74ae2f95809460c4b231289be174b00496d24
Chidera Ozigbo <82067806+Chideraozigbo@users.noreply.github.com>
2024-06-22 00:03:31 +0100

    Update install_airflow.sh

--- install_airflow.sh
+++ install_airflow.sh
@@ -15,7 +15,7 @@ source airflow_env/bin/activate
 pip install --upgrade pip
 
 # Define Airflow version and Python version
-AIRFLOW_VERSION=2.6.3
+AIRFLOW_VERSION=2.1.3
 PYTHON_VERSION=$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 
